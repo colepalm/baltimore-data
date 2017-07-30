54ea7a37f1f9babca58111017206bd0852c2ba6c
Cole Palm <palm.cole@gmail.com>
2017-07-29 18:28:04 -0700

    get running

---
+++ src/main/java/com/contrast/BaltimoreData.java
@@ -0,0 +1,29 @@
+package com.contrast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@SpringBootApplication
+@RestController
+public class BaltimoreData {
+
+    // Match everything without a suffix (so not a static resource)
+    @RequestMapping(value = "/{[path:[^\\.]*}")
+    public ModelAndView redirect() {
+        // Forward to home page so that route is preserved.
+        return new ModelAndView("forward:/");
+    }
+
+    @RequestMapping(value = "/grocery")
+    public Integer getGroceryStores()
+    {
+        return 20;
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaltimoreData.class, args);
+	}
+}
